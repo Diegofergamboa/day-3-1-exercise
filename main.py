18063a6b589455456e29f8c66c925062c26ae4dd
Diegofergamboa <diegofergamboa@hotmail.es>
2021-03-11 19:36:25 +0000

    Final done!

--- main.py
+++ main.py
@@ -8,6 +8,13 @@ if number % 2 == 1:
 	print('The number is odd')
 else:
 	print('The number is even')
+#It´s important to know that the modulo gives 0 when the number is even and 1 when the number is odd
+#So, another way to code this challenge is:
+
+if number % 2 == 0:
+	print('Even')
+else:
+	print('Odd')
 
 
 
